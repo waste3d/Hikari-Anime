0415c03526c688c8dcde15605e57f5dead4b3a62
waste3d <kolechkapk@gmail.com>
2025-10-22 15:17:12 +0300

    feat: movies handlers (gateway)

--- gateway/cmd/api/main.go
+++ gateway/cmd/api/main.go
@@ -29,6 +29,8 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/api/v1/movies/popular", getPopularMoviesHandler(metadataServiceClient))
+	router.GET("/api/v1/movies/search", searchMoviesHandler(metadataServiceClient))
+	router.GET("/api/v1/movies/:id", movieByIDHandler(metadataServiceClient))
 
 	err = router.Run(gatewayPort)
 	if err != nil {
@@ -42,8 +44,8 @@ func getPopularMoviesHandler(metadataServiceClient pb.MetadataServiceClient) fun
 		language := c.Query("language")
 
 		if page == "" || language == "" {
-			c.DefaultQuery("page", "1")
-			c.DefaultQuery("language", "en-EN")
+			page = c.DefaultQuery("page", "1")
+			language = c.DefaultQuery("language", "ru-RU")
 		}
 
 		pageInt, err := strconv.Atoi(page)
@@ -64,6 +66,67 @@ func getPopularMoviesHandler(metadataServiceClient pb.MetadataServiceClient) fun
 			return
 		}
 
+		c.JSON(http.StatusOK, response)
+	}
+}
+
+func searchMoviesHandler(metadataServiceClient pb.MetadataServiceClient) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		query := c.Query("query")
+		page := c.Query("page")
+		language := c.Query("language")
+
+		if page == "" || language == "" {
+			page = c.DefaultQuery("page", "1")
+			language = c.DefaultQuery("language", "ru-RU")
+		}
+
+		pageInt, err := strconv.Atoi(page)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid page parameter"})
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		response, err := metadataServiceClient.SearchMovies(ctx, &pb.SearchRequest{
+			Query:    query,
+			Page:     int32(pageInt),
+			Language: language,
+		})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to search movies"})
+			return
+		}
+
+		c.JSON(http.StatusOK, response)
+	}
+}
+
+func movieByIDHandler(client pb.MetadataServiceClient) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		idStr := c.Param("id")
+		language := c.DefaultQuery("language", "ru-RU")
+
+		idInt, err := strconv.ParseInt(idStr, 10, 64)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid movie ID parameter"})
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		response, err := client.GetMovieByID(ctx, &pb.GetMovieByIDRequest{
+			MovieId:  idInt,
+			Language: language,
+		})
+		if err != nil {
+			log.Printf("ошибка при вызове GetMovieByID: %v", err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get movie by ID"})
+			return
+		}
 		c.JSON(http.StatusOK, response)
 	}
 }
